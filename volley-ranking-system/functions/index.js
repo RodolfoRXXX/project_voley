0914a1756c61643dbe522dba59b9bc41dd866089
Rodolfo <rodolfo.singular@gmail.com>
2026-06-16 10:40:12 -0300

    update

--- volley-ranking-system/functions/index.js
+++ volley-ranking-system/functions/index.js
@@ -3,12 +3,26 @@ require("./src/events/bootstrap");
 //const admin = require("firebase-admin");
 //admin.initializeApp();
 
+const envFlag = (name, defaultValue = true) => {
+  const raw = process.env[name];
+  if (raw === undefined) return defaultValue;
+  return String(raw).toLowerCase() === "true";
+};
+
+const ENABLE_ON_MATCH_DEADLINE = envFlag("ENABLE_ON_MATCH_DEADLINE", true);
+const ENABLE_ON_MATCH_START = envFlag("ENABLE_ON_MATCH_START", true);
+const ENABLE_ON_PENDING_ALERTS_MAINTENANCE = envFlag("ENABLE_ON_PENDING_ALERTS_MAINTENANCE", true);
+
 // Exportar triggers directamente
 exports.onUserCreate = require("./src/triggers/onUserCreate");
 exports.onParticipationCreate = require("./src/triggers/onParticipationCreate");
 exports.onParticipationUpdate = require("./src/triggers/onParticipationUpdate");
-exports.onMatchDeadline = require("./src/triggers/onMatchDeadline");
-exports.onMatchStart = require("./src/triggers/onMatchStart");
+if (ENABLE_ON_MATCH_DEADLINE) {
+  exports.onMatchDeadline = require("./src/triggers/onMatchDeadline");
+}
+if (ENABLE_ON_MATCH_START) {
+  exports.onMatchStart = require("./src/triggers/onMatchStart");
+}
 exports.onMatchClose = require("./src/triggers/onMatchClose");
 exports.onUserPendingAlertsSync = require("./src/triggers/onUserPendingAlertsSync");
 exports.onGroupPendingAlertsSync = require("./src/triggers/onGroupPendingAlertsSync");
@@ -16,7 +30,9 @@ exports.onTournamentPendingAlertsSync = require("./src/triggers/onTournamentPend
 exports.onTournamentRegistrationPendingAlertsSync = require("./src/triggers/onTournamentRegistrationPendingAlertsSync");
 exports.onTournamentMatchPendingAlertsSync = require("./src/triggers/onTournamentMatchPendingAlertsSync");
 exports.onTournamentTeamPendingAlertsSync = require("./src/triggers/onTournamentTeamPendingAlertsSync");
-exports.onPendingAlertsMaintenance = require("./src/triggers/onPendingAlertsMaintenance");
+if (ENABLE_ON_PENDING_ALERTS_MAINTENANCE) {
+  exports.onPendingAlertsMaintenance = require("./src/triggers/onPendingAlertsMaintenance");
+}
 exports.completeOnboarding = require("./callables/completeOnboarding");
 exports.getFormaciones = require("./callables/getFormaciones");
 exports.createMatch = require("./callables/createMatch");

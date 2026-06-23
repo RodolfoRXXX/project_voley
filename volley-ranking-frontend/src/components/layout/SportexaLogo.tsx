e47445ee845df622021b72bc55fd512757f60458
Rodolfo <rodolfo.singular@gmail.com>
2026-06-23 12:56:44 -0300

    update

--- volley-ranking-frontend/src/components/layout/SportexaLogo.tsx
+++ volley-ranking-frontend/src/components/layout/SportexaLogo.tsx
@@ -11,7 +11,7 @@ export default function SportexaLogo() {
       href="/"
       className="sportexa-logo md:absolute md:left-1/2 md:-translate-x-1/2"
     >
-      <span className="logo-text">sporte</span>
+      <span className="logo-text">Sporte</span>
       <span className="logo-x">X</span>
       <span className="logo-text">a</span>
     </Link>
